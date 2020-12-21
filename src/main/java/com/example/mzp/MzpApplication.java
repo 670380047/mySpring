8e26859c269b96f0552544ad49a3c4c1224fb60a
mzp <670380047@qq.com>
2020-12-21 13:52:27 +0800

    第一次提交2020年12月21日 13:52:09

---
+++ src/main/java/com/example/mzp/MzpApplication.java
@@ -0,0 +1,13 @@
+package com.example.mzp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mzp/MzpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mzp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
